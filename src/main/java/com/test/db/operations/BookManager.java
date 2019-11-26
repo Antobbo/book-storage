ef9824ef53fe790d4add5585ca191a249d4c5300
Antobbo <antobbo@gmail.com>
2019-11-26 21:44:55 +0000

    changes to main class and bookmanager

--- src/main/java/com/test/db/operations/BookManager.java
+++ src/main/java/com/test/db/operations/BookManager.java
@@ -92,4 +92,5 @@ public class BookManager {
     	session.getTransaction().commit();
 	    session.close();
     }
+    
 }
--- src/main/java/com/test/main/BookMainApp.java
+++ src/main/java/com/test/main/BookMainApp.java
@@ -1,17 +1,23 @@
 package com.test.main;
 
 import com.test.db.operations.BookManager;
+import com.test.userInteraction.UserInput;
 
 public class BookMainApp {
 
 	public static void main(String[] args) {
 		BookMainApp bookMainApp = new BookMainApp();
 		BookManager bookManager = new BookManager();
+		UserInput userInput = new UserInput();
 		bookManager.setup();
 		//bookManager.create();
 		//bookManager.read();
 		//bookManager.update();
 		//bookManager.delete();
+		
+		
+		int userChoice = userInput.getUserInput();
+		userInput.processChoice(userChoice, bookManager);
 		bookManager.exit();
 		
 
