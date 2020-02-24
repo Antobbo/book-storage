059be8ac49277473aec5a28a564c762261bb0085
Antonio Borrillo Borrillo <antobbo@gmail.com>
2020-02-24 21:49:10 +0000

    Removed commented out code

--- src/main/java/com/test/db/operations/BookManager.java
+++ src/main/java/com/test/db/operations/BookManager.java
@@ -54,32 +54,12 @@ public class BookManager {
     public void read(String dbQuery, String field) {
         // code to get a book
     	Session session = sessionFactory.openSession();
-    	
-    	
-//    	Criteria criteria = session.createCriteria(Book.class);
-//    	    
-//    	Book uniqueResult = (Book) criteria.add(Restrictions.eq(field, dbQuery)).uniqueResult();
-//    	List list = criteria.add(Restrictions.eq(field, dbQuery)).list();
-//		
-//		if(uniqueResult == null)
-//		{
-//			System.out.println(String.format(Messages.NO_RECORD_FOUND, dbQuery));
-//		}
-//		else
-//		{
-//			String message = String.format(Messages.RECORD_FOUND, dbQuery, uniqueResult);
-//    		System.out.println(message);
-//		}
-    	
-		//https://www.concretepage.com/hibernate-4/hibernate-4-criteria-query-tutorials-with-examples-using-jpa-2
     	CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();    	
     	CriteriaQuery<Book> criteriaQuery  = criteriaBuilder.createQuery(Book.class);
     	Root<Book> bookRoot = criteriaQuery.from(Book.class);
     	
-    	criteriaQuery.select(bookRoot);
-    	
-    	criteriaQuery.where(criteriaBuilder.equal(bookRoot.get(field),dbQuery));    	
-    	
+    	criteriaQuery.select(bookRoot);    	
+    	criteriaQuery.where(criteriaBuilder.equal(bookRoot.get(field),dbQuery));
     	List<Book> resultList = session.createQuery(criteriaQuery).getResultList();
     	
     	if(resultList.isEmpty())
@@ -88,13 +68,10 @@ public class BookManager {
     	}
     	else
     	{
-    		//String message = String.format(Messages.RECORD_FOUND, dbQuery, null);
-    		System.out.println(resultList.size());	
     		for(Book book : resultList)
     		{
     			System.out.println(book);
-    		}
-    		
+    		}    		
     	}
     	
     	String queryString = session.createQuery(criteriaQuery ).getQueryString();
@@ -113,69 +90,18 @@ public class BookManager {
     	session.createQuery(update).executeUpdate();
     	session.getTransaction().commit();
 	    session.close();
-    	
-    	
-//    	Criteria criteria = session.createCriteria(Book.class);
-//    	Book uniqueResult = (Book) criteria.add(Restrictions.eq(field, dbQuery)).uniqueResult();
-//    	if(uniqueResult == null)
-//		{
-//			System.out.println(String.format(Messages.NO_RECORD_FOUND, dbQuery));
-//		}
-//    	else
-//    	{    	
-//	    	switch(field)
-//	    	{
-//	    		case "author":    			
-//	    			uniqueResult.setAuthor(modifiedField);
-//	    			break;
-//	    		case "title":    			
-//	    			uniqueResult.setTitle(modifiedField);
-//	    			break;
-//	    		case "location":    			
-//	    			uniqueResult.setLocation(Integer.parseInt(modifiedField));
-//	    			break;    			
-//	    	}
-//	    	
-//	    	session.update(uniqueResult);
-//	    	session.getTransaction().commit();
-//		    session.close();
-//		    System.out.println(uniqueResult);
-//    	}
     }
  
     public void delete(String dbQuery, String field) {
         // code to remove a book
     	Session session = sessionFactory.openSession();
     	session.beginTransaction();
-//    	Criteria criteria = session.createCriteria(Book.class);
-//    	Book uniqueResult = (Book) criteria.add(Restrictions.eq(field, dbQuery)).uniqueResult();
-//    	if(uniqueResult == null)
-//		{
-//			System.out.println(String.format(Messages.NO_RECORD_FOUND, dbQuery));
-//		}
-//    	else
-//    	{
-//	    	session.delete(uniqueResult);
-//	    	session.getTransaction().commit();
-//		    session.close();
-//    	}
-    	
     	CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();    	
     	CriteriaDelete<Book> delete = criteriaBuilder.createCriteriaDelete(Book.class);
     	Root<Book> bookRoot = delete.from(Book.class);
     	delete.where(criteriaBuilder.equal(bookRoot.get(field),dbQuery));
     	session.createQuery(delete).executeUpdate();
     	session.getTransaction().commit();
-	    session.close();
-    	
-//    	createCriteriaDelete.where(bookRoot)
-//    	
-//    	criteriaQuery.select(bookRoot);
-//    	
-//    	criteriaQuery.where(criteriaBuilder.equal(bookRoot.get(field),dbQuery));  
-    	
-    	
-    	
-    }
-    
+	    session.close();    	
+    }  
 }
